7d960efa5ccbe2a7fd3f93ff6896e7c90da89977
CDIM17 <56546970+CDIM17@users.noreply.github.com>
2021-08-27 19:46:44 -0400

    Update query.sql

--- config/query.sql
+++ config/query.sql
@@ -1,13 +1,3 @@
-CREATE TABLE `comentarios` (
-  `id_comentario` int(11) NOT NULL AUTO_INCREMENT,
-  `id_pelicula` int(11),
-  `comentario` varchar(100),
-  `fecha` datetime,
-  `nombre` varchar(30),
-  `correo` varchar(100),
-    PRIMARY KEY(id_comentario)
-) ENGINE=InnoDB;
-
 CREATE TABLE usuarios (
   id_usuario int(11) NOT NULL AUTO_INCREMENT,
   usuario varchar(30),
@@ -36,3 +26,29 @@ CREATE TABLE peliculas (
   imagen_bg text,
   PRIMARY KEY(id_pelicula)
 ) ENGINE=InnoDB;
+
+
+
+INSERT INTO `administradores` (`id_admin`, `usuario`, `pass`, `correo`) VALUES
+(1, 'admin', '12345', 'admin@admin.com'),
+(3, 'admin1', '12346', 'admin1@admin1.com');
+
+--
+-- Dumping data for table `peliculas`
+--
+
+INSERT INTO `peliculas` (`id_pelicula`, `nombre`, `genero`, `descripcion`, `fecha`, `trailer`, `link_pelicula`, `imagen`, `imagen_bg`) VALUES
+(9, 'Spiderman', 'Accion', 'Hombre Aracnido', NULL, 'https://www.youtube.com/watch?v=rl0EZCJcrGc', 'img/spider.jpg', 'spider.jpg', NULL),
+(10, 'Fast and Furious', 'Accion', 'Pelicula de Carros', NULL, 'https://www.youtube.com/watch?v=rl0EZCJcrGc', 'img/fast-furious-9-fotogramas-1618418035.jpg', 'fast-furious-9-fotogramas-1618418035.jpg', NULL),
+(11, 'Batman', 'Ciencia Ficcion', 'Ciudad Gotica', NULL, 'https://www.youtube.com/watch?v=YNl8yV8oPZM', 'img/batman.webp', 'batman.webp', NULL),
+(12, 'John Wick', 'Accion ', 'Dangerous Man', NULL, 'https://www.youtube.com/watch?v=rPCzao7H6n8', 'img/John Wick.jpg', 'John Wick.jpg', NULL),
+(13, 'Transformers', 'Ciencia Ficcion', 'Pelicula de Robots', NULL, 'https://www.youtube.com/watch?v=ET9SHYzMz_4&t=2s', 'img/transformers-830x483.jpg.jfif', 'transformers-830x483.jpg.jfif', NULL),
+(14, 'Avengers', 'Ciencia Ficcion', 'Pelicula de Heroes', NULL, 'https://www.youtube.com/watch?v=eOrNdBpGMv8', 'img/The_Avengers_Poster.png', 'The_Avengers_Poster.png', NULL);
+
+--
+-- Dumping data for table `usuarios`
+--
+
+INSERT INTO `usuarios` (`id_usuario`, `usuario`, `pass`, `correo`) VALUES
+(2, 'cdim', '12345', 'cdim@cdim.com');
+COMMIT;
